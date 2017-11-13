4c226166292387da380342c3661c13b63aa2c87e
Duskprime <dusk95@naver.com>
2017-11-13 17:37:58 +0900

    11.13
    패턴추가

--- 3-2/Assets/Script/Boss.cs
+++ 3-2/Assets/Script/Boss.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Boss : MonoBehaviour {
 
     public GameObject PlayerPos;
+    public GameObject groundStinger; //땅에서 나오는 가시
 
     Animator ani;
     Player player;
@@ -12,9 +13,11 @@ public class Boss : MonoBehaviour {
 
     public float Boss_HP = 2000f;
     int RandPattern;
+    int StingerNum;
 
     bool HozCheck;
     bool RushCheck;
+    bool UprisingCheck;
 
     public enum BOSSSTATE { SLEEP, IDLE, UPRISING, RUSH, SHOUT, SHOUT2, WAIT, MOVE, DEATH }
     BOSSSTATE bossstate = BOSSSTATE.IDLE;
@@ -22,7 +25,7 @@ public class Boss : MonoBehaviour {
     // Use this for initialization
     void Awake () {
         StartCoroutine(FSM());
-        //StartCoroutine(Pattern());
+        StartCoroutine(Pattern());
         player = GameObject.Find("Player").GetComponent<Player>();
         ani = GetComponent<Animator>();
 	}
@@ -40,20 +43,23 @@ public class Boss : MonoBehaviour {
                     StartCoroutine(BossMove());
                     break;
 
+                case BOSSSTATE.MOVE:
+                    break;
+
                 case BOSSSTATE.UPRISING: // 솟아오르기 (땅속으로 사라진 뒤 1초 후 플레이어가 있던 위치에 솟아오름)
-                   
+                    StartCoroutine(Uprising(PlayerPos.transform.position));
+                    bossstate = BOSSSTATE.WAIT;
                     break;
 
                 case BOSSSTATE.RUSH: // 덮치기 (플레이어가 있는 방향으로 빠르게 이동)
-                    ani.SetBool("Rushing", true);
+                    StartCoroutine(Rush_P());
+                    bossstate = BOSSSTATE.WAIT;
                     break;
 
                 case BOSSSTATE.SHOUT: // 포효1 (플레이어가 서있는 위치에 0.8초마다 가시가 솟아오름)
-                    
-                    break;
-
-                case BOSSSTATE.SHOUT2: // 포효2 (포효하는 입에서 가시가 나와 플레이어에게 날아감)
-                   
+                    ani.SetBool("Shoting", true);
+                    StingerNum = 0;
+                    bossstate = BOSSSTATE.WAIT;
                     break;
 
                 case BOSSSTATE.DEATH:
@@ -72,7 +78,7 @@ public class Boss : MonoBehaviour {
     {
         if (bossstate == BOSSSTATE.IDLE)
         {
-            RandPattern = Random.Range(0, 4);
+            RandPattern = Random.Range(0, 3);
             if (RandPattern == 0) // 덮치기 
             {
                 bossstate = BOSSSTATE.RUSH;
@@ -88,13 +94,37 @@ public class Boss : MonoBehaviour {
                 bossstate = BOSSSTATE.SHOUT;
             }
 
-            else if (RandPattern == 3) //포효 2 
-            {
-                bossstate = BOSSSTATE.SHOUT2;
-            }
         }
         yield return new WaitForSeconds(4f);
         StartCoroutine(Pattern());
+    }
+
+
+    void bossSpikeing()
+    {
+        ani.SetBool("Shoting", false);
+        ani.SetBool("Spikeing", true);
+        Vector3 StingerPos = new Vector3(PlayerPos.transform.position.x, -34.96701f, PlayerPos.transform.position.z);
+        StartCoroutine(Shout_Stinger(StingerPos));
+    }
+
+
+    IEnumerator Shout_Stinger(Vector3 pos)
+    {
+        StingerNum += 1;
+        yield return new WaitForSeconds(0.8f);
+        Instantiate(groundStinger, pos, Quaternion.identity);
+        if(StingerNum < 5)
+        {
+            StartCoroutine(Shout_Stinger(new Vector3(PlayerPos.transform.position.x, -34.96701f, PlayerPos.transform.position.z)));
+        }
+
+        else if(StingerNum == 5)
+        {
+            ani.SetBool("Spikeing", false);
+            yield return new WaitForSeconds(0.8f);
+            bossstate = BOSSSTATE.IDLE;
+        }
     }
 
     IEnumerator BossMove() // 보스 움직임
@@ -136,7 +166,6 @@ public class Boss : MonoBehaviour {
         Vector3 RushVec = Vector3.zero;
         Vector3 playerPos = player.transform.position;
         var t = 0f;
-
         do
         {
             if (HozCheck)
@@ -162,6 +191,22 @@ public class Boss : MonoBehaviour {
             ani.SetBool("Rushing", false);
             bossstate = BOSSSTATE.IDLE;
         }
+    }
+
+    IEnumerator Uprising(Vector3 End) // 들어갔다 나오기
+    {
+        Debug.Log("D");
+        ani.SetBool("Digging", true);
+        yield return new WaitForSeconds(2f);
+        transform.position = new Vector3(End.x, transform.position.y + 20, transform.position.z);
+        ani.SetBool("Digging", false);
+        yield return new WaitForSeconds(0.5f);
+        bossstate = BOSSSTATE.IDLE;
+    }
+
+    public void Boss_Dig() //들어가는 이미지가 끝난후 아래로 내려준다.
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y - 20, transform.position.z);
     }
 
 
